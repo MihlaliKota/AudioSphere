b84d3f2b64bfba9bbff2c9faf0bcb06a3ecfb894
MihlaliKota <hlalikota@gmail.com>
2023-11-02 22:29:11 +0200

    Restarting Application

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,8 @@
 import "./App.css";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Carousel from "./components/Carousel/Carousel";
-import Main from "./components/Main/Main";
-
 function App() {
   return (
     <div>
-      <Header />
-      <Carousel />
-      <Main />
-      <Footer />
+
     </div>
   );
 }
