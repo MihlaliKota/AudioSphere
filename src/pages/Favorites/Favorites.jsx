b2688e2f0153f7bb40db9930ba033a7900882886
MihlaliKota <hlalikota@gmail.com>
2023-11-12 17:49:18 +0200

    Added back button to Favorites

--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom";
+import "./Favorites.css";
+
 const Favorites = () => {
+  const goBack = useNavigate();
+
   return (
     <div>
+      <p className="back-links" onClick={() => goBack("/home")}>
+        Back
+      </p>
       <h1>Favorite</h1>
     </div>
   );
