c5eeba6dea179ba866aa86691f3fac2a1abaf460
MihlaliKota <hlalikota@gmail.com>
2023-12-05 20:17:57 +0200

    Removed Favorites page to configure

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header/Header";
 import Home from "./pages/Home/Home";
 import Podcast from "./pages/Podcast/Podcast";
 // import Login from "./pages/Login/Auth";
-import Favorites from "./pages/Favorites/Favorites";
+// import Favorites from "./pages/Favorites/Favorites";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           {/* <Route path="/" element={<Login />} /> */}
           <Route path="/" element={<Home />} />
           <Route path="/podcast/:id" element={<Podcast />} />
-          <Route path="/favorites" element={<Favorites />} />
+          {/* <Route path="/favorites" element={<Favorites />} /> */}
           <Route path="*" element={<Home />} />
         </Routes>
         <Footer />
