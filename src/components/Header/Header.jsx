ea75f2855f6b10574ef51f2bf1d705449db38020
MihlaliKota <hlalikota@gmail.com>
2025-04-23 16:15:14 +0200

    Update navigation link to point to home and replace Supabase client URL in Auth component

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <nav className="header">
-      <Link className="Link" to="/">
+      <Link className="Link" to="/home">
         <div className="logo-container">
           <svg
             className="logo"
--- src/pages/Login/Auth.jsx
+++ src/pages/Login/Auth.jsx
@@ -5,8 +5,8 @@ import "./Auth.css";
 import { useNavigate } from "react-router-dom";
 
 const supabase = createClient(
-  "https://kseelwrthsxzdpawaurv.supabase.co",
-  " eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzZWVsd3J0aHN4emRwYXdhdXJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTkxMzUzMzAsImV4cCI6MjAxNDcxMTMzMH0.anI3qWt4RnDHaZdmxF7VNuIgtFOUIdVw-Tgr277JQfI"
+  "https://xrcocrcbjzdubctpstzq.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhyY29jcmNianpkdWJjdHBzdHpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMzkyODUsImV4cCI6MjA2MDkxNTI4NX0.O1ehydWoyDCY2OM91bskMxPQ-Ej_XpqWaTX68k7AuXA"
 );
 
 const Login = () => {
